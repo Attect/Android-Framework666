d2710738f4dd6a2cbff5a1adeb6dca236bd72fab
attect <412966721@qq.com>
2019-06-21 21:30:10 +0800

    Resources:各单位转换

--- framework666/src/main/java/studio/attect/framework666/extensions/Resources.kt
+++ framework666/src/main/java/studio/attect/framework666/extensions/Resources.kt
@@ -1,6 +1,8 @@
 package studio.attect.framework666.extensions
 
 import android.content.res.Resources
+import android.util.TypedValue
+import kotlin.math.roundToInt
 
 /**
  * 系统动画时间：短
@@ -19,3 +21,83 @@ val Resources.systemMediumAnimTime: Long
  */
 val Resources.systemLongAnimTime: Long
     get() = getInteger(android.R.integer.config_longAnimTime).toLong()
+
+/**
+ * dp转像素
+ */
+fun Resources.dp2px(dp: Float): Int {
+    return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, displayMetrics).roundToInt()
+}
+
+/**
+ * 像素转dp
+ */
+fun Resources.px2dp(px: Int): Float {
+    return px / displayMetrics.density
+}
+
+/**
+ * sp转像素
+ * 只用与字体
+ * 与用户设置有关
+ */
+fun Resources.sp2px(sp: Float): Int {
+    return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, sp, displayMetrics).roundToInt()
+}
+
+/**
+ * 像素转sp
+ * 只用于字体
+ * 与用户设置有关
+ */
+fun Resources.px2sp(px: Int): Float {
+    return px / displayMetrics.scaledDensity
+}
+
+/**
+ * pt转像素
+ * 字体尺寸
+ * 1pt为1/72英寸
+ */
+fun Resources.pt2px(pt: Float): Int {
+    return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_PT, pt, displayMetrics).roundToInt()
+}
+
+/**
+ * 像素转pt
+ * 字体尺寸
+ * 1pt为1/72英寸
+ */
+fun Resources.px2pt(px: Int): Float {
+    return px / (displayMetrics.xdpi * (1.0f / 72))
+}
+
+/**
+ * 英寸转像素
+ * 1英寸约为2.54厘米
+ */
+fun Resources.in2px(inch: Float): Int {
+    return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_IN, inch, displayMetrics).roundToInt()
+}
+
+/**
+ * 像素转英寸
+ * 1英寸约为2.54厘米
+ */
+fun Resources.px2in(px: Int): Float {
+    return px / displayMetrics.xdpi
+}
+
+/**
+ * 毫米转像素
+ */
+fun Resources.mm2px(mm: Float): Int {
+    return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_MM, mm, displayMetrics).roundToInt()
+}
+
+/**
+ * 像素转毫米
+ */
+fun Resources.px2mm(px: Int): Float {
+    return px / (displayMetrics.xdpi * (1.0f / 25.4f))
+}
