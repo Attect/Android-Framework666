a99e6210706b37c8a12dfd1a5ec8fcd95d9db5da
attect <412966721@qq.com>
2019-07-30 14:51:18 +0800

    String扩展移除一个不泛用的扩展

--- framework666/src/main/java/studio/attect/framework666/extensions/String.kt
+++ framework666/src/main/java/studio/attect/framework666/extensions/String.kt
@@ -4,24 +4,6 @@ package studio.attect.framework666.extensions
  * 此文件针对Java/Kotlin的String进行方法扩展
  */
 
-/**
- * 将字符串转换为针对TTS发音友好的内容
- * 转换后的内容不适合作为显示内容
- */
-fun String.coverToTTSFriendlyString(): String {
-    var result = this
-    val noRegex = Regex("No.[0-9]+")
-    result = noRegex.replace(result) {
-        it.value.replaceArabicNumber2ChineseNumberForTTS()
-    }
-    val haoRegex = Regex("号:[0-9]+")
-    result = haoRegex.replace(result) {
-        it.value.replaceArabicNumber2ChineseNumberForTTS()
-    }
-    result = result.replace("No.", "编号为")
-    result = result.replace("￥", "元")
-    return result
-}
 
 /**
  * 将字符串中应该单个念出的数字转换格式使其一个一个数字的读出
