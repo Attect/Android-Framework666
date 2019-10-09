33fc79d2b0b404b76a5c64e2e0ccca34660e5058
attect <412966721@qq.com>
2019-10-09 19:22:10 +0800

    增加View.setRealtimeClickListener

--- framework666/src/main/java/studio/attect/framework666/extensions/View.kt
+++ framework666/src/main/java/studio/attect/framework666/extensions/View.kt
@@ -2,6 +2,7 @@ package studio.attect.framework666.extensions
 
 import android.content.Context
 import android.os.ResultReceiver
+import android.view.MotionEvent
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 
@@ -36,4 +37,20 @@ fun View.hitTest(x: Int, y: Int): Boolean {
     val bottom = bottom + ty
 
     return (x >= left) && (x <= right) && (y >= top) && (y <= bottom)
+}
+
+/**
+ * 非常快速的点击响应
+ * 就在用户触碰屏幕的一瞬间
+ *
+ * 通过setOnTouchListener实现，并且不会阻塞其逻辑运行，因为要确保波纹动画的进行
+ * 因此使用时切记不要再对View设置普通的onClickListener和setOnLongClickListener以及setOnTouchListener
+ */
+fun View.setRealtimeClickListener(onClickListener: View.OnClickListener) {
+    setOnTouchListener { v, event ->
+        if (event.action == MotionEvent.ACTION_DOWN) {
+            onClickListener.onClick(this)
+        }
+        false
+    }
 }
