b86f34a9ac1a14cfd27d746a1f2cc7b09736e0c3
attect <412966721@qq.com>
2019-10-14 11:02:35 +0800

    MutableLiveData的update方法提供返回参数配置是否触发观察

--- framework666/src/main/java/studio/attect/framework666/extensions/MutableLiveData.kt
+++ framework666/src/main/java/studio/attect/framework666/extensions/MutableLiveData.kt
@@ -5,8 +5,9 @@ import androidx.lifecycle.MutableLiveData
 /**
  * 数据变更（引用未变更）
  * 触发观察
+ *
+ * 可决定是否更新
  */
-fun <T> MutableLiveData<T>.update(update: (T?) -> Unit) {
-    update.invoke(value)
-    value = value
+fun <T> MutableLiveData<T>.update(update: (T?) -> Boolean) {
+    if (update.invoke(value)) value = value
 }
