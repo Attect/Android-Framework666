3695acaebdc60622ab2016c0a3cafa1a6eebec26
attect <412966721@qq.com>
2019-10-17 10:24:21 +0800

    MutableLiveData增加updateIfNotNull扩展方法

--- framework666/src/main/java/studio/attect/framework666/extensions/MutableLiveData.kt
+++ framework666/src/main/java/studio/attect/framework666/extensions/MutableLiveData.kt
@@ -11,3 +11,16 @@ import androidx.lifecycle.MutableLiveData
 fun <T> MutableLiveData<T>.update(update: (T?) -> Boolean) {
     if (update.invoke(value)) value = value
 }
+
+/**
+ * 数据变更（引用未变更）
+ * 触发观察
+ * 数据不为null时操作
+ *
+ * 可决定是否更新
+ */
+fun <T> MutableLiveData<T>.updateIfNotNull(update: (T) -> Boolean) {
+    value?.let {
+        if (update(it)) value = value
+    }
+}
