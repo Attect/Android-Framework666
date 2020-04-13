4b621b4ab9db731964323b5b36e0a4384b7ead87
attect <412966721@qq.com>
2020-04-13 11:34:54 +0800

    Mutable Pair/Triple

---
+++ framework666/src/main/java/studio/attect/framework666/model/MutablePair.kt
@@ -0,0 +1,17 @@
+package studio.attect.framework666.model
+
+/**
+ * Represents a generic pair of two values.
+ *
+ * There is no meaning attached to values in this class, it can be used for any purpose.
+ * Pair exhibits value semantics, i.e. two pairs are equal if both components are equal.
+ *
+ *
+ * @param A type of the first value.
+ * @param B type of the second value.
+ * @property first First value.
+ * @property second Second value.
+ * @constructor Creates a new instance of Pair.
+ */
+data class MutablePair<A, B>(var first: A, var second: B) {
+}
