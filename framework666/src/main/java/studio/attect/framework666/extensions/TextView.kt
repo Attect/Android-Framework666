30afcc7253cb4b48a96198dbfca97da93ed968ec
attect <412966721@qq.com>
2020-07-13 11:07:22 +0800

    TextView提供一个设置多个文字点击的扩展方法

--- framework666/src/main/java/studio/attect/framework666/extensions/TextView.kt
+++ framework666/src/main/java/studio/attect/framework666/extensions/TextView.kt
@@ -1,7 +1,10 @@
 package studio.attect.framework666.extensions
 
 import android.os.Build
-import android.text.Html
+import android.text.*
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.view.View
 import android.widget.TextView
 
 /**
@@ -23,4 +26,26 @@ fun TextView.setHtml(html: String?, flags: Int = 2) {
     } else {
         Html.fromHtml(html)
     }
+}
+
+/**
+ * 设置文字点击
+ * @see <a href="https://stackoverflow.com/a/45727769">How to set the part of the text view is clickable</a>
+ */
+fun TextView.makeTextClick(vararg links: Pair<String, View.OnClickListener>) {
+    val spannableString = SpannableString(this.text)
+    for (link in links) {
+        val clickableSpan = object : ClickableSpan() {
+            override fun onClick(view: View) {
+                Selection.setSelection((view as TextView).text as Spannable, 0)
+                view.invalidate()
+                link.second.onClick(view)
+            }
+        }
+        val startIndexOfLink = this.text.toString().indexOf(link.first)
+        spannableString.setSpan(clickableSpan, startIndexOfLink, startIndexOfLink + link.first.length,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+    }
+    this.movementMethod = LinkMovementMethod.getInstance() // without LinkMovementMethod, link can not click
+    this.setText(spannableString, TextView.BufferType.SPANNABLE)
 }
